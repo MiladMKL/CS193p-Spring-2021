dde7ad943eaf06ca8978d01b15f37554132925d7
Milad MKL <miladmiyakheel@gmail.com>
2022-02-17 16:32:11 +0100

    Lecture 2

---
+++ Memorize_Lecture2/Memorize_Lecture2/Memorize_Lecture2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_Lecture2App.swift
+//  Memorize_Lecture2
+//
+//  Created by Milad on 17.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_Lecture2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
